7e571e3b0aab880fc7d7b590e6e7dc67e8716c2b
Jamie Rushford <jar.rushford@gmail.com>
2019-01-22 22:01:03 -0700

    Plan out endpoints

--- server.js
+++ server.js
@@ -11,6 +11,29 @@ app.use((request, response, next) => {
   response.status(404).send('Page Not Found')
 })
 
+app.get('/api/v1/projects', (request, response) => {
+  // Get all projects
+})
+
+app.get('/api/v1/projects/:id/palettes', (request, response) => {
+  // Get all palettes associated with a project
+})
+
+app.get('/api/v1/projects/:id/palettes/:palette', (request, response) => {
+  // Get a specific palette when clicked on
+})
+
+app.post('/api/v1/projects/:id', (request, response) => {
+  // Post a new palette associated with a project
+})
+
+app.delete('/api/v1/projects/:id/palettes/:palette', (request, response) => {
+  // Delete a palette from a project
+})
+
+
+// (Delete a project?)
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
 })
